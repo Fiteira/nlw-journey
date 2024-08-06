b8c31aeba2f0c524143c3863ca0f675cd086b766
Fiteira <leandrofiteira@hotmail.com>
2024-08-06 11:33:43 +0100

    feat: confirm participants, trip

--- backend/src/routes/confirm-trip.ts
+++ backend/src/routes/confirm-trip.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import nodemailer from 'nodemailer';
 
 export async function conformTrip(app : FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', {
@@ -9,9 +12,64 @@ export async function conformTrip(app : FastifyInstance) {
                 tripId : z.string().uuid()
             })
             
-        }},async ( request ) => {
-        
-            return { tripId : request.params.tripId };
+        }},async ( request , response) => {
+            
+            const { tripId } = request.params;
+
+            const trip = await prisma.trip.findUnique({
+                where : {
+                    id : tripId
+                },
+                include : {
+                    participants : {
+                        where : {
+                            is_owner : false
+                        }
+                    }
+                }
+            });
+
+            if(!trip) {
+                throw new Error("Trip not found");
+            }
+
+            if(trip.is_confirmed){
+                return response.redirect(`http://localhost:3000/trips/${tripId}`);
+            }
+
+            await prisma.trip.update({
+                where : {
+                    id : tripId
+                },
+                data : {
+                    is_confirmed : true
+                }
+            }); 
+
+            const mail = await getMailClient();
+            
+            await Promise.all(
+                trip.participants.map(async participant => {
+
+                const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
+
+                const message = await mail.sendMail({
+                    from : {
+                        name: 'Team plann.er',
+                        address : 'oi@plann.er'
+                    },
+                    to : participant.email,
+                    subject : 'Testing sending email',
+                    html : `<p>Hi ${participant.name},</p> <p> Your trip to ${trip.destination} 
+                    has been created. </p> <p> You can access it <a href=${confirmationLink}>Confirmation Link</a></p>`.trim()
+                    });
+
+                console.log(nodemailer.getTestMessageUrl(message));
+
+                })
+            );
+
+            return response.redirect(`http://localhost:3000/trips/${tripId}`);
         }
     );
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,11 @@ import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { conformTrip } from "./routes/confirm-trip";
+import { conformParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLink } from "./routes/get-links";
 
 const app = fastify();
 
@@ -15,6 +20,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(conformTrip);
+app.register(conformParticipant);
+app.register(createActivity);  
+app.register(getActivities);
+app.register(createLink);
+app.register(getLink);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
